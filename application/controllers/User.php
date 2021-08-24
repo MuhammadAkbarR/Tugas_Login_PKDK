cd1592b04f776f2bf59b5dfddebdd0dde7d069a1
AbunAkbar <maasakbar111@gmail.com>
2021-08-25 00:19:32 +0700

    25/08/2021

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -8,7 +8,8 @@ class User extends CI_Controller
     {
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
-        echo'Selamat Datang ' . $data['user']['name'];
+      
+        $this->load->view('user/index', $data);
     }
 
 }
