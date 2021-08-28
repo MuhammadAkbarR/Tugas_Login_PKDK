cfb722f1a5d76fba5e6430ff775788f57a082397
AbunAkbar <maasakbar111@gmail.com>
2021-08-28 23:34:42 +0700

    28/08/20021

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,19 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
 
     public function index()
     {
-        $data['title'] = 'My Profile';
+        $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
       
         $this->load->view('templates/header.php', $data);
         $this->load->view('templates/sidebar.php', $data);
         $this->load->view('templates/topbar.php', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/footer.php');
        
     }
--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -44,7 +44,11 @@ class Auth extends CI_Controller
 
                   ];
                   $this->session->set_userdata($data);
-                  redirect('user');
+                  if ($user['role_id'] == 1){
+                     redirect('admin');
+                  }else {
+                     redirect('user');
+                  }
 
             }else{
                $this->session->set_flashdata('message' , '<div class="alert alert-danger" 
